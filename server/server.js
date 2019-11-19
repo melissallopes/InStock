1f9a1943c0f4847e3ff1705b2a2cb091ff0c5812
kevin <chengyu0718@hotmail.com>
2019-11-19 13:47:33 -0800

    bug fixes

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
 //ENTRY point for my application
 
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
+const logger = require('./middleware/logger');
 const app = express();
 
+app.use(cors());
+
+app.use(logger);
 app.use(express.json());
 
-app.use("/api/locations", require("./routes/api/locations"));
-app.use("/api/inventory", require("./routes/api/inventory"));
+app.use('/api/locations', require('./routes/api/locations'));
+app.use('/api/inventory', require('./routes/api/inventory'));
 
 app.listen(5000, () => {
-  console.log("listening");
+	console.log('listening');
 });
