e3049602fe5298479cb7e0377d5f47fbc15f7035
a7jamali@gmail.com <Ali@Dominics-iPhone.localdomain>
2019-11-20 11:46:01 -0800

    backup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
 import { Switch, Route, Redirect } from 'react-router-dom';
 
-=======
-import { Switch, Route } from 'react-router-dom';
->>>>>>> master
 import Header from './Components/Header';
 import Inventory from './Components/Inventory';
 import Locations from './Components/Locations';
@@ -13,13 +9,10 @@ import LocationDetail from './Components/LocationsDetail';
 function App() {
 	return (
 		<div className="app">
-			<Header />
+			<Header/>
 			<Switch>
-<<<<<<< HEAD
-			<Redirect from="/" to="/inventory" exact component={Inventory}/>
-				<Route path="/inventory" component={Inventory} />
-				<Route path="/locations" exact component={Locations} />
-=======
+
+				<Redirect from="/" to="/inventory" exact component={Inventory}/>
 				<Route path="/" exact component={Inventory} />
 				<Route path="/locations" exact component={Locations} />
 				<Route
@@ -30,7 +23,7 @@ function App() {
 						</div>
 					)}
 				/>
->>>>>>> master
+
 			</Switch>
 		</div>
 	);
