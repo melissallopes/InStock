4445945ed6ae4b0b70e7fdb6d6dfba63910a09b5
kevin <chengyu0718@hotmail.com>
2019-11-19 16:18:57 -0800

    location page mobile now done

--- client/src/Components/Locations.js
+++ client/src/Components/Locations.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+import LocationsMap from './LocationsMap';
+
 export default class Locations extends Component {
+	state = {
+		locations: []
+	};
+
+	componentDidMount = () => {
+		axios.get('http://localhost:5000/locations').then((res) => {
+			this.setState({
+				locations: res.data
+			});
+		});
+	};
+
 	render() {
-		return <div>LOC</div>;
+		return (
+			<div className="locations">
+				<div className="locations__header">
+					<h2 className="locations__header-title">locations</h2>
+					<input type="text" className="locations__header-input" placeholder="Search" />
+				</div>
+				<LocationsMap data={this.state} />
+			</div>
+		);
 	}
 }
