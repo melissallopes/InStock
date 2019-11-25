665107cf75ad95ac4d1e331911cfad3ec450edef
Melissa Lima Lopes <melissalimalopes@Julias-iPhone.localdomain>
2019-11-25 15:10:44 -0800

    edit page and remove buttons

--- client/src/Components/Locations.js
+++ client/src/Components/Locations.js
@@ -1,169 +1,228 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import LocationsMapSmall from './LocationsMap';
-import LocationsMapBig from './LocationsMapDesktop';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import axios from "axios";
+import LocationsMapSmall from "./LocationsMap";
+import LocationsMapBig from "./LocationsMapDesktop";
+import { Link } from "react-router-dom";
 
-import Plus from '../assets/Icons/SVG/Icon-add.svg';
+import Plus from "../assets/Icons/SVG/Icon-add.svg";
 
 export default class Locations extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			locations: [],
-			isDesktop: false,
-			isTablet: false,
-			display: 'none',
-			value: 'select',
-			checked: false,
-			optionName: [],
-			selectedOption: null
-		};
-		this.updatePredicate = this.updatePredicate.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      locations: [],
+      isDesktop: false,
+      isTablet: false,
+      display: "none",
+      value: "select",
+      checked: false,
+      optionName: [],
+      selectedOption: null
+    };
+    this.updatePredicate = this.updatePredicate.bind(this);
+  }
 
-	componentDidMount = () => {
-		this.updatePredicate();
-		window.addEventListener('resize', this.updatePredicate);
+  componentDidMount = () => {
+    this.updatePredicate();
+    window.addEventListener("resize", this.updatePredicate);
 
-		axios.get('http://localhost:5000/locations').then((res) => {
-			this.setState({
-				locations: res.data
-			});
-		});
-	};
+    axios.get("http://localhost:5000/locations").then(res => {
+      this.setState({
+        locations: res.data
+      });
+    });
+  };
 
-	componentWillUnmount() {
-		window.removeEventListener('resize', this.updatePredicate);
-	}
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updatePredicate);
+  }
 
-	updatePredicate() {
-		this.setState({
-			isDesktop: window.innerWidth > 1439,
-			isTablet: window.innerWidth > 767
-		});
-	}
+  updatePredicate() {
+    this.setState({
+      isDesktop: window.innerWidth > 1439,
+      isTablet: window.innerWidth > 767
+    });
+  }
 
-	toggleDisplay = () => {
-		if (this.state.display === 'none') {
-			this.setState({
-				display: 'flex'
-			});
-		} else {
-			this.setState({
-				display: 'none'
-			});
-		}
-	};
+  toggleDisplay = () => {
+    if (this.state.display === "none") {
+      this.setState({
+        display: "flex"
+      });
+    } else {
+      this.setState({
+        display: "none"
+      });
+    }
+  };
 
-	onSubmit = () => {
-		axios.post('http://localhost:5000/locations', {
-			warehouse: this.warehouseName.value,
-			street: `${this.street.value}, ${this.city.value}, ${this.country.value} `,
-			city: this.city.value,
-			country: this.country.value,
-			number: this.number.value,
-			email: this.email.value,
-			categories1: this.category.value,
-			position: this.position.value,
-			actualCity: this.city.value,
-			actualCountry: this.country.value,
-			shortStreet: this.street.value
-		});
-	};
+  onSubmit = () => {
+    axios.post("http://localhost:5000/locations", {
+      warehouse: this.warehouseName.value,
+      street: `${this.street.value}, ${this.city.value}, ${this.country.value} `,
+      city: this.city.value,
+      country: this.country.value,
+      number: this.number.value,
+      email: this.email.value,
+      categories1: this.category.value,
+      position: this.position.value,
+      actualCity: this.city.value,
+      actualCountry: this.country.value,
+      shortStreet: this.street.value
+    });
+  };
 
-	render() {
-		const isDesktop = this.state.isDesktop;
-		const isTablet = this.state.isTablet;
+  render() {
+    const isDesktop = this.state.isDesktop;
+    const isTablet = this.state.isTablet;
 
-		return (
-			<div>
-				<div className="locations">
-					<div className="locations__header">
-						<h2 className="locations__header-title">locations</h2>
-						<input type="text" className="locations__header-input" placeholder="Search" />
-					</div>
-					<div>
-						{isDesktop ? <LocationsMapBig data={this.state} /> : <LocationsMapSmall data={this.state} />}
-					</div>
+    return (
+      <div>
+        <div className="locations">
+          <div className="locations__header">
+            <h2 className="locations__header-title">locations</h2>
+            <input
+              type="text"
+              className="locations__header-input"
+              placeholder="Search"
+            />
+          </div>
+          <div>
+            {isDesktop ? (
+              <LocationsMapBig data={this.state} />
+            ) : (
+              <LocationsMapSmall data={this.state} />
+            )}
+          </div>
 
-					{isTablet ? (
-						<button type="button" className="locations-button" onClick={this.toggleDisplay}>
-							<img src={Plus} alt="upload" className="locations-button-img" />
-						</button>
-					) : (
-						<Link to="/location/new">
-							<button type="button" className="locations-button" onClick={this.toggleDisplay}>
-								<img src={Plus} alt="upload" className="locations-button-img" />
-							</button>
-						</Link>
-					)}
+          {isTablet ? (
+            <button
+              type="button"
+              className="locations-button"
+              onClick={this.toggleDisplay}
+            >
+              <img src={Plus} alt="upload" className="locations-button-img" />
+            </button>
+          ) : (
+            <Link to="/location/new">
+              <button
+                type="button"
+                className="locations-button"
+                onClick={this.toggleDisplay}
+              >
+                <img src={Plus} alt="upload" className="locations-button-img" />
+              </button>
+            </Link>
+          )}
 
-					<div className="uploadBig" style={{ display: `${this.state.display}` }}>
-						<div className="uploadBig-inner">
-							<h2>create new</h2>
-							<div className="uploadBig-div">
-								<div className="uploadBig-smallDiv">
-									<h3>warehouse name</h3>
-									<input
-										type="text"
-										placeholder="Warehouse Name"
-										ref={(ref) => (this.warehouseName = ref)}
-									/>
-								</div>
-								<div className="uploadBig-smallDiv">
-									<h3>street</h3>
-									<input type="text" placeholder="Street" ref={(ref) => (this.street = ref)} />
-								</div>
-							</div>
-							<div className="uploadBig-div">
-								<div className="uploadBig-smallDiv">
-									<h3>city</h3>
-									<input type="text" placeholder="City" ref={(ref) => (this.city = ref)} />
-								</div>
-								<div className="uploadBig-smallDiv">
-									<h3>country</h3>
-									<input type="text" placeholder="Country" ref={(ref) => (this.country = ref)} />
-								</div>
-							</div>
-							<div className="uploadBig-cate">
-								<h3>categories</h3>
-								<input type="text" placeholder="Category" ref={(ref) => (this.category = ref)} />
-							</div>
-							<h2>contact</h2>
-							<div className="uploadBig-div">
-								<div className="uploadBig-smallDiv">
-									<h3>name</h3>
-									<input type="text" placeholder="Name" ref={(ref) => (this.personName = ref)} />
-								</div>
-								<div className="uploadBig-smallDiv">
-									<h3>postition</h3>
-									<input type="text" placeholder="Posititon" ref={(ref) => (this.position = ref)} />
-								</div>
-							</div>
-							<div className="uploadBig-div">
-								<div className="uploadBig-smallDiv">
-									<h3>number</h3>
-									<input type="text" placeholder="Number" ref={(ref) => (this.number = ref)} />
-								</div>
-								<div className="uploadBig-smallDiv">
-									<h3>email</h3>
-									<input type="text" placeholder="Email" ref={(ref) => (this.email = ref)} />
-								</div>
-							</div>
-							<div>
-								<button type="button" className="newPage-save" onClick={this.onSubmit}>
-									save
-								</button>
+          <div
+            className="uploadBig"
+            style={{ display: `${this.state.display}` }}
+          >
+            <div className="uploadBig-inner">
+              <h2>create new</h2>
+              <div className="uploadBig-div">
+                <div className="uploadBig-smallDiv">
+                  <h3>warehouse name</h3>
+                  <input
+                    type="text"
+                    placeholder="Warehouse Name"
+                    ref={ref => (this.warehouseName = ref)}
+                  />
+                </div>
+                <div className="uploadBig-smallDiv">
+                  <h3>street</h3>
+                  <input
+                    type="text"
+                    placeholder="Street"
+                    ref={ref => (this.street = ref)}
+                  />
+                </div>
+              </div>
+              <div className="uploadBig-div">
+                <div className="uploadBig-smallDiv">
+                  <h3>city</h3>
+                  <input
+                    type="text"
+                    placeholder="City"
+                    ref={ref => (this.city = ref)}
+                  />
+                </div>
+                <div className="uploadBig-smallDiv">
+                  <h3>country</h3>
+                  <input
+                    type="text"
+                    placeholder="Country"
+                    ref={ref => (this.country = ref)}
+                  />
+                </div>
+              </div>
+              <div className="uploadBig-cate">
+                <h3>categories</h3>
+                <input
+                  type="text"
+                  placeholder="Category"
+                  ref={ref => (this.category = ref)}
+                />
+              </div>
+              <h2>contact</h2>
+              <div className="uploadBig-div">
+                <div className="uploadBig-smallDiv">
+                  <h3>name</h3>
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    ref={ref => (this.personName = ref)}
+                  />
+                </div>
+                <div className="uploadBig-smallDiv">
+                  <h3>postition</h3>
+                  <input
+                    type="text"
+                    placeholder="Posititon"
+                    ref={ref => (this.position = ref)}
+                  />
+                </div>
+              </div>
+              <div className="uploadBig-div">
+                <div className="uploadBig-smallDiv">
+                  <h3>number</h3>
+                  <input
+                    type="text"
+                    placeholder="Number"
+                    ref={ref => (this.number = ref)}
+                  />
+                </div>
+                <div className="uploadBig-smallDiv">
+                  <h3>email</h3>
+                  <input
+                    type="text"
+                    placeholder="Email"
+                    ref={ref => (this.email = ref)}
+                  />
+                </div>
+              </div>
+              <div>
+                <button
+                  type="button"
+                  className="newPage-save"
+                  onClick={this.onSubmit}
+                >
+                  save
+                </button>
 
-								<button type="button" className="newPage-cancel" onClick={this.toggleDisplay}>
-									cancel
-								</button>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
+                <button
+                  type="button"
+                  className="newPage-cancel"
+                  onClick={this.toggleDisplay}
+                >
+                  cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
--- server/routes/api/inventory.js
+++ server/routes/api/inventory.js
@@ -1,48 +1,77 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const inventoryFile = __dirname + '/../../model/inventory.json';
+var path = require("path");
+const inventoryFile = path.join(__dirname, "../../model", "inventory.json");
+// const inventoryFile = __dirname + "/../../model/inventory.json";
+const invent = path.join(__dirname, "../../model", "invent.json");
 
 let inventory = require(inventoryFile);
-const helper = require('../../helper/helper');
+const helper = require("../../helper/helper");
 
-//  Get all inventories
-router.get('/', (req, res) => {
-	res.json(inventory);
+//  GET all inventories
+router.get("/", (req, res) => {
+  res.json(inventory);
 });
 
-router.get('/:item', (req, res) => {
-	const found = inventory.some((inventory) => inventory.item === req.params.item);
-	if (found) {
-		res.json(inventory.filter((inventory) => inventory.item === req.params.item));
-	} else {
-		res.status(400).json({ errorMessage: `Item:${req.params.item} not found` });
-	}
+router.get("/:item", (req, res) => {
+  const found = inventory.some(inventory => inventory.item === req.params.item);
+  if (found) {
+    res.json(inventory.filter(inventory => inventory.item === req.params.item));
+  } else {
+    res.status(400).json({ errorMessage: `Item:${req.params.item} not found` });
+  }
 });
 
-router.post('/', (req, res) => {
-	const newItem = {
-		item: req.body.item,
-		ordered: req.body.ordered,
-		location: req.body.location,
-		quantity: req.body.quantity,
-		status: req.body.status,
-		description: req.body.description
-	};
-	inventory.push(newItem);
-	helper.writeJSONFile(inventoryFile, inventory);
-	res.json(inventory);
+//POST
+
+router.post("/", (req, res) => {
+  const newItem = {
+    item: req.body.item,
+    ordered: req.body.ordered,
+    location: req.body.location,
+    quantity: req.body.quantity,
+    status: req.body.status,
+    description: req.body.description
+  };
+  inventory.push(newItem);
+  helper.writeJSONFile(inventoryFile, inventory);
+  res.json(inventory);
 });
 
-router.delete('/', (req, res) => {
-	const found = inventory.some((inventor) => inventor.name === req.params.name);
-	if (found) {
-		const itemAfterDeletion = inventory.filter((inventor) => inventor.name === req.param.name);
-		helper.writeJSONFile(inventoryFile, itemAfterDeletion);
-		res.json({
-			msg: `Item with name: ${req.params.name} Deleted`,
-			inventory: itemAfterDeletion
-		});
-	}
+//PUT
+router.put("/:item", (req, res) => {
+  const requestItem = req.params.item;
+  const product = inventory.filter(product => {
+    return product.item == requestItem;
+  });
+
+  const index = inventory.indexOf(product[0]);
+  //check to see which of the keys are coming in
+  const keys = Object.keys(req.body);
+  keys.forEach(key => {
+    product[0][key] = req.body[key];
+  });
+
+  inventory[index] = product[0];
+
+  helper.writeJSONFile(inventoryFile, inventory);
+  res.json(inventory);
+});
+
+//DELETE
+
+router.delete("/", (req, res) => {
+  const found = inventory.some(inventor => inventor.name === req.params.name);
+  if (found) {
+    const itemAfterDeletion = inventory.filter(
+      inventor => inventor.name === req.param.name
+    );
+    helper.writeJSONFile(inventoryFile, itemAfterDeletion);
+    res.json({
+      msg: `Item with name: ${req.params.name} deleted`,
+      inventory: itemAfterDeletion
+    });
+  }
 });
 
 module.exports = router;
