5372507dddd7f0c308a31878c0800ebc379d7b33
a7jamali@gmail.com <Ali@Alis-MBP-4.vc.shawcable.net>
2019-11-26 00:18:26 -0800

    commit Nov26

--- client/src/Components/Locations.js
+++ client/src/Components/Locations.js
@@ -104,35 +104,6 @@ export default class Locations extends Component {
 							</Link>
 						)}
 						<div className="newBigPage" style={{ display: `${this.state.display}` }}>
-<<<<<<< HEAD
-						<div className="newBigPage-flex">
-							<h2>create new</h2>
-							<form>
-								<h3>product</h3>
-								<input type="text" placeholder="Item Name" className="newPage-input" />
-								<h3>last ordered</h3>
-								<input type="text" placeholder="yyyy-mm-dd" className="newPage-input" />
-								<h3>city</h3>
-								<input type="text" placeholder="City" className="newPage-input" />
-								<h3>country</h3>
-								<select>{allCountry}</select>
-								<h3>quantity</h3>
-								<input type="text" placeholder="0" className="newPage-input" />
-								<h3>status</h3>
-								<div className="newPage-instock">
-									<h4>in stock</h4>
-									<input className="apple-switch" type="checkbox" />
-								</div>
-								<h3>item description</h3>
-								<input type="text" placeholder="(optional)" className="newPage-inputBox" />
-								<div>
-									<button type="button" className="newPage-save">
-										save
-									</button>
-									<Link to="/">
-										<button type="button" className="newPage-cancel">
-											cancel
-=======
 							<div className="newBigPage-tablet">
 								<h2>create new</h2>
 								<form>
@@ -156,7 +127,6 @@ export default class Locations extends Component {
 									<div>
 										<button type="button" className="newPage-save">
 											save
->>>>>>> master
 										</button>
 										<Link to="/">
 											<button type="button" className="newPage-cancel">
@@ -169,7 +139,6 @@ export default class Locations extends Component {
 						</div>
 					</div>
 					</div>
-				</div>
 			);
 		} else {
 			return (
